0844c08906c0601f3dc0c580c2284e4fa98e966e
Andrey <s1avyan27@gmail.com>
2020-05-05 13:02:37 +0300

    Добалвена карта с координатами. некоторые фиксы css

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ for (let anchor of anchors) {
   });
 }
 // плавная прокрутка
+
+// Google map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 48.39, lng: 35.022 },
+    zoom: 15,
+  });
+}
+//  /google map
+
+// API - application programming interface
+console.log("Privet");
